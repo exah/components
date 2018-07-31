0d35487767a17841a17cd4225ff5e46fd96c61eb
John Grishin <r@exah.ru>
2018-07-31 10:39:07 +0300

    📝 Update docs for `Layout` and `MediaObject`

--- doczrc.js
+++ doczrc.js
@@ -9,6 +9,11 @@ export default {
   themeConfig: createTheme({
     space: {
       default: [ 0, 8, 16, 32, 64 ]
+    },
+    color: {
+      black: '#000',
+      gray: [ '#eee', '#ddd', '#ccc', '#bbb', '#aaa' ],
+      white: '#fff'
     }
   })
 }
