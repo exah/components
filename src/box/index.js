01779013eb6a8ceb558447d17d625e27854c4588
Ivan Grishin <r@exah.me>
2019-02-19 20:20:12 +0300

    ✨ Add `css` prop to `BorderBox`

--- src/box/index.js
+++ src/box/index.js
@@ -2,9 +2,10 @@ import styled from '@emotion/styled'
 import { system } from 'pss'
 import { Base } from '../base'
 
-const BorderBox = styled(Base)`
-  box-sizing: border-box;
-`
+const BorderBox = styled(Base)(
+  { boxSizing: 'border-box' },
+  (props) => props.css
+)
 
 BorderBox.displayName = 'BorderBox'
 BorderBox.propTypes = { ...Base.propTypes }
