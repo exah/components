38847dcf152b658b69e61863cec9cf023ac2f9e2
John Grishin <r@exah.ru>
2019-04-24 00:39:53 +0300

    ♻️ Extend `Image` from `Box`

--- src/image/index.js
+++ src/image/index.js
@@ -1,50 +1,17 @@
-import {
-  combineStyles,
-  cursor,
-  display,
-  flex,
-  boxSelfAlignment,
-  order,
-  margin,
-  opacity,
-  position,
-  borderRadius,
-  sizes,
-  transform
-} from 'pss'
+import React from 'react'
+import { Box } from '../box'
 
-import styled from '@emotion/styled'
-import { createBase, blacklistOf } from '../utils'
-
-const styles = combineStyles(
-  cursor,
-  display,
-  flex,
-  boxSelfAlignment,
-  order,
-  margin,
-  opacity,
-  position,
-  borderRadius,
-  sizes,
-  transform
-)
-
-const ImageBase = createBase('img', {
-  blacklist: blacklistOf(styles)
-})
-
-const Image = styled(ImageBase)(
-  { display: 'block', maxWidth: '100%', height: 'auto' },
-  styles
+const Image = (props) => (
+  <Box
+    as='img'
+    display='block'
+    maxWidth='100%'
+    height='auto'
+    {...props}
+  />
 )
 
 Image.displayName = 'Image'
-
-Image.propTypes = {
-  ...ImageBase.propTypes,
-  ...styles.propTypes
-}
 
 export {
   Image
