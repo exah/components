a5faf299679ec0bfc08a7a7ddb40f649fe6e4b52
Ivan Grishin <r@exah.me>
2019-04-23 12:32:26 +0300

    ✨ Add reset styles to `Text`

--- src/text/index.js
+++ src/text/index.js
@@ -1,13 +1,21 @@
 import styled from '@emotion/styled'
+import { css } from '@emotion/core'
 import { text, textStyle } from 'pss'
 import { createBase, blacklistOf } from '../utils'
 import { Box } from '../box'
+
+const reset = css`
+  color: inherit;
+  font: inherit;
+  text-decoration: none;
+`
 
 const TextBase = createBase(Box, {
   blacklist: blacklistOf(text)
 })
 
 const Text = styled(TextBase)(
+  reset,
   textStyle.variant,
   text
 )
