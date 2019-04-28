c32710e30f1aad68da5a09ba6f323bffa1dd041c
John Grishin <r@exah.ru>
2019-04-28 14:51:31 +0300

    ✨ Add `listStyle` prop to `List` and `List.Item`

--- src/list.js
+++ src/list.js
@@ -1,20 +1,33 @@
-import React from 'react'
 import styled from '@emotion/styled'
+import { style } from 'pss'
 import { Box } from './box'
 import { Flex } from './flex'
 
-const List = (props) => (
-  <Flex
-    as='ul'
-    flexDirection='column'
-    {...props}
-  />
-)
-
-const ListItem = styled(Box)({
-  listStyle: 'none'
+const listStyle = style({
+  cssProp: 'listStyle'
 })
 
+const List = styled(Flex)(
+  listStyle
+)
+
+List.propTypes = {
+  ...listStyle.propTypes,
+  ...Flex.propTypes
+}
+
+List.defaultProps = {
+  as: 'ul',
+  flexDirection: 'column',
+  listStyle: 'none',
+  ...Flex.defaultProps
+}
+
+const ListItem = styled(Box)(
+  listStyle
+)
+
+ListItem.propTypes = { ...listStyle.propTypes, ...Box.propTypes }
 ListItem.defaultProps = { as: 'li' }
 List.Item = ListItem
 
