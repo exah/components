13c731ac9edd7c08def8dad80264adb60683fb0e
John Grishin <r@exah.ru>
2019-05-09 02:56:23 +0300

    ♻️ Remove `extend` utility

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { filterObj, isStr, flatten, identity } from '@exah/utils'
+import { filterObj, isStr, identity } from '@exah/utils'
 
 export const omit = (blacklist = []) => filterObj((key) => !blacklist.includes(key))
 export const omitStyles = (styles) => omit(Object.keys(Object(styles.propTypes)))
-export const cx = (...args) => flatten(args).filter(isStr).join(' ')
 
 export const withRef = (render) => React.forwardRef(
   (props, ref) => render({ ref, ...props })
@@ -39,25 +38,6 @@ export function base ({
         PropTypes.elementType,
         PropTypes.arrayOf(PropTypes.elementType)
       ])
-    }
-  })
-}
-
-const generateClassName = ((id) => () => {
-  id++
-  return 'pss-' + id
-})(-1)
-
-export function extend (render) {
-  const className = generateClassName()
-
-  const Comp = withRef((props) =>
-    render({ ...props, className: cx(props.className, className) })
-  )
-
-  return Object.defineProperties(Comp, {
-    toString: {
-      value: () => `.${className}`
     }
   })
 }
