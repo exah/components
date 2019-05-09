17788d46091d9ec6c6cf7b017bab7496e79e8a02
Ivan Grishin <r@exah.me>
2019-05-09 18:37:29 +0300

    ➖ Remove `@babel/runtime`

--- babel.config.js
+++ babel.config.js
@@ -4,19 +4,24 @@ const IS_ESM = !IS_TEST && process.env.MODULES !== 'cjs'
 
 module.exports = {
   presets: [
-    '@babel/preset-react',
+    [
+      '@babel/preset-react',
+      {
+        useBuiltIns: true,
+        development: IS_DEV
+      }
+    ],
     [
       '@babel/preset-env',
       {
         modules: IS_ESM ? false : 'commonjs',
-        loose: true
+        loose: true,
+        useBuiltIns: 'entry',
+        corejs: 3
       }
     ]
   ],
   plugins: [
-    '@babel/plugin-proposal-class-properties',
-    [ '@babel/plugin-transform-runtime', { useESModules: IS_ESM } ],
-    [ '@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true, loose: true } ],
     [ 'emotion', { sourceMap: IS_DEV } ]
   ]
 }
