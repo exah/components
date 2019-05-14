00b48b07e366f96dbce98600f94d490b54242903
John Grishin <r@exah.ru>
2019-05-14 10:43:39 +0300

    ♻️ Remove key from  `MatchMedia`, wrap `Feed` in styled

--- src/feed.js
+++ src/feed.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types'
-import React, { useMemo } from 'react'
+import React, { forwardRef, useMemo } from 'react'
 import styled from '@emotion/styled'
 import { fallbackTo, initArr } from '@exah/utils'
 import { DEFAULT_GRID } from './constants'
 import { useMatchMediaContext } from './match-media'
 import { FlexGrid } from './flex-grid'
 import { Box } from './box'
+import { omit } from './utils'
 
 const groupChildren = (items = [], length = 3) =>
   items.reduce((target, value, index) => {
@@ -28,21 +29,20 @@ function useGroupChildren (size, children) {
   }, [ size, children ])
 }
 
-function Feed ({ children, grid, column, ...rest }) {
-  const matchedMedia = useMatchMediaContext()
+const FeedContainer = forwardRef(({ children, grid, column, ...rest }, ref) => {
+  const mediaKey = useMatchMediaContext().matches.find((key) => column[key])
 
   const columnForMedia = fallbackTo(
-    column[matchedMedia.key],
+    column[mediaKey],
     column.all,
     column,
     grid
   )
 
-  const groupSize = grid / columnForMedia
-  const childrenGroups = useGroupChildren(groupSize, children)
+  const childrenGroups = useGroupChildren(grid / columnForMedia, children)
 
   return (
-    <FlexGrid spaceTarget={Feed.Item} {...rest} columns={grid}>
+    <FlexGrid ref={ref} spaceTarget={Feed.Item} {...rest} columns={grid}>
       {childrenGroups.map((child, index) => (
         <FlexGrid.Item key={index} column={columnForMedia}>
           {child}
@@ -50,24 +50,31 @@ function Feed ({ children, grid, column, ...rest }) {
       ))}
     </FlexGrid>
   )
-}
+})
 
-Feed.displayName = 'Feed'
+FeedContainer.displayName = 'Feed'
 
-Feed.propTypes = {
+FeedContainer.propTypes = {
   grid: PropTypes.number.isRequired,
   column: PropTypes.oneOfType([
     PropTypes.number,
     PropTypes.objectOf(PropTypes.number)
-  ]).isRequired
+  ]).isRequired,
+  ...omit([ 'columns' ])(FlexGrid.propTypes)
 }
 
-Feed.defaultProps = {
+FeedContainer.defaultProps = {
   grid: DEFAULT_GRID,
   column: DEFAULT_GRID
 }
 
-Feed.Item = styled(Box)()
+const Feed = styled(FeedContainer)()
+
+Feed.propTypes = { ...FeedContainer.propTypes }
+
+const FeedItem = styled(Box)()
+
+Feed.Item = FeedItem
 Feed.Item.displayName = 'Feed.Item'
 
 export {
