ff1971179ff9e48ee52d21389160eef2308ae098
John Grishin <r@exah.ru>
2019-05-14 10:09:05 +0300

    ♻️ Use styled to create `Layout` components

--- src/layout.js
+++ src/layout.js
@@ -1,23 +1,27 @@
-import React from 'react'
+import styled from '@emotion/styled'
 import { Flex } from './flex'
 import { Box } from './box'
-import { withRef } from './utils'
 
-const Layout = withRef((props) => (
-  <Flex flex='1 1 auto' {...props} />
-))
-
-Layout.Side = withRef((props) => (
-  <Box flex='0 0 auto' {...props} />
-))
-
-Layout.Content = withRef((props) => (
-  <Box flex='1 1 auto' {...props} />
-))
+const Layout = styled(Flex)()
 
 Layout.displayName = 'Layout'
-Layout.Side.displayName = 'Layout.Side'
-Layout.Content.displayName = 'Layout.Content'
+Layout.propTypes = { ...Flex.propTypes }
+Layout.defaultProps = { ...Flex.defaultProps, flex: '1 1 auto' }
+
+const LayoutSide = styled(Box)()
+
+LayoutSide.displayName = 'Layout.Side'
+LayoutSide.propTypes = { ...Box.propTypes }
+LayoutSide.defaultProps = { flex: '0 0 auto' }
+
+const LayoutContent = styled(Box)()
+
+LayoutContent.displayName = 'Layout.Content'
+LayoutContent.propTypes = { ...Box.propTypes }
+LayoutContent.defaultProps = { flex: '1 1 auto' }
+
+Layout.Side = LayoutSide
+Layout.Content = LayoutContent
 
 export {
   Layout
